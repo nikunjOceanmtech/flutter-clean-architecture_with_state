877575d0498085f240cb7b96f68ae5341a8d39b3
nikunjOceanmtech <147715598+nikunjOceanmtech@users.noreply.github.com>
2023-11-02 13:29:56 +0530

    update
    update

--- src/templates/cubit.template.ts
+++ src/templates/cubit.template.ts
@@ -1,37 +1,41 @@
 import * as changeCase from "change-case";
 
-export function getCubitTemplate (cubitName: string, useEquatable: boolean): string {
+export function getCubitTemplate(cubitName: string, useEquatable: boolean): string {
   return useEquatable
     ? getEquatableCubitTemplate(cubitName)
     : getDefaultCubitTemplate(cubitName);
 }
 
-function getEquatableCubitTemplate (cubitName: string) {
+function getEquatableCubitTemplate(cubitName: string) {
   const pascalCaseCubitName = changeCase.pascalCase(cubitName.toLowerCase());
   const snakeCaseCubitName = changeCase.snakeCase(cubitName.toLowerCase());
-  const cubitState = `${pascalCaseCubitName}State`;
+  const cubitState = `${ pascalCaseCubitName }State`;
   return `import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
-part '${snakeCaseCubitName}_state.dart';
+part '${ snakeCaseCubitName }_state.dart';
 
-class ${pascalCaseCubitName}Cubit extends Cubit<${cubitState}> {
-  ${pascalCaseCubitName}Cubit() : super(${pascalCaseCubitName}Initial());
+class ${ pascalCaseCubitName }Cubit extends Cubit<${ cubitState }> {
+  ${ pascalCaseCubitName }Cubit() : super(${ pascalCaseCubitName }Initial());
 }
 `;
 }
 
-function getDefaultCubitTemplate (cubitName: string) {
+function getDefaultCubitTemplate(cubitName: string) {
   const pascalCaseCubitName = changeCase.pascalCase(cubitName.toLowerCase());
   const snakeCaseCubitName = changeCase.snakeCase(cubitName.toLowerCase());
-  const cubitState = `${pascalCaseCubitName}State`;
+  const cubitState = `${ pascalCaseCubitName }State`;
   return `import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part '${snakeCaseCubitName}_state.dart';
+part '${ snakeCaseCubitName }_state.dart';
 
-class ${pascalCaseCubitName}Cubit extends Cubit<${cubitState}> {
-  ${pascalCaseCubitName}Cubit() : super(${pascalCaseCubitName}Initial());
+class ${ pascalCaseCubitName }Cubit extends Cubit<${ cubitState }> {
+  ${ pascalCaseCubitName }Cubit() : super(${ pascalCaseCubitName }LodingState());
+
+  void loadData(){
+    
+  }
 }
 `;
 }
